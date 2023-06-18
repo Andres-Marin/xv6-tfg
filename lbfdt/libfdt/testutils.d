b4d983471370ce78655750ef525cb34a184c0140
Andres-Marin <71381401+Andres-Marin@users.noreply.github.com>
2023-06-18 20:09:41 +0200

    Delete lbfdt directory

--- lbfdt/libfdt/testutils.d
+++
@@ -1,3 +0,0 @@
-libfdt/testutils.o libfdt/testutils.d: libfdt/testutils.c libfdt/libfdt.h \
- libfdt/libfdt_env.h libfdt/fdt.h libfdt/tests.h libfdt/util.h \
- libfdt/testdata.h libfdt/../libfdt/libfdt_internal.h
--- lbfdt/libfdt/util.d
+++
@@ -1,2 +0,0 @@
-libfdt/util.o libfdt/util.d: libfdt/util.c libfdt/libfdt.h \
- libfdt/libfdt_env.h libfdt/fdt.h libfdt/util.h
